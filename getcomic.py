d726eaa17bdb0b87a773e4931c822cfb427bb38f
Michael Pomogajko <m.pomogajko@gmail.com>
2017-06-14 17:47:31 +0200

    yo

--- getcomic.py
+++ getcomic.py
@@ -6,6 +6,7 @@ from random import randint
 from bs4 import BeautifulSoup
 from urllib import urlretrieve
 import time
+
 number = randint(1, 1850)
 
 r = requests.get("https://xkcd.com/%d/" %number)
@@ -13,5 +14,6 @@ src = "https:" + BeautifulSoup(r.text, "lxml").find(id="comic").find("img")["src
 
 urlretrieve(src, "pic.png")
 envoy.run("open pic.png")
-time.sleep(1)
-envoy.run("rm pic.png")
+
+#time.sleep(1)
+#envoy.run("rm pic.png")
